af87739b690aacf40209b0c4f528ebe4dafce5fd
GabrielJ-SO <gabrielodisi20@gmail.com>
2025-07-15 11:27:47 -0300

    Adiciona opção de escolher tabuada

--- Tabuada.cpp
+++ Tabuada.cpp
@@ -1,19 +1,69 @@
 #include <iostream>
+#include <clocale>
 using namespace std;
 
 int main()
 {
-    int operando1, operando2, resultado;
+    system("chcp 65001"); // muda o código do console para UTF-8
 
-    for (operando1 = 1; operando1 <= 10; operando1++) {
-		    cout << "Tabuada do: " << operando1 << endl;
-            for (operando2 = 0; operando2 <= 10; operando2++) {
+    float operando1, operando2, resultado;
+    char opcao;
+	bool continuar = true;
+
+
+    while (continuar)
+    {
+        cout << "\tCALCULAR TABUADA" << endl;
+        cout << "O que você prefere?" << endl;
+        cout << "1. Gerar a tabuada do 1 ao 10." << endl;
+	    cout << "2. Gerar a tabuada de um número específico." << endl;
+        cout << "3. Sair." << endl;
+	    cout << "Digite a opção desejada: ";
+        cin >> opcao;
+
+
+        switch (opcao)
+        {
+        case '1':
+            system("cls");
+            cout << "\tTabuada do 1 ao 10:" << endl << endl;
+            for (operando1 = 1; operando1 <= 10; operando1++) {
+                cout << "Tabuada do: " << operando1 << endl;
+                for (operando2 = 0; operando2 <= 10; operando2++) {
                     resultado = operando1 * operando2;
-					cout << operando1 << " x " << operando2 << " = " << resultado << endl;
-        }
-            cout << endl;
-    }
+                    cout << operando1 << " x " << operando2 << " = " << resultado << endl;
+                }
+                cout << endl;
+            }
+            break;
 
+        case '2':
+            system("cls");
+            cout << "Digite o número para gerar a tabuada:" << endl;
+            cout << "Número: ";
+            cin >> operando1;
+
+            cout << "\tTabuada do " << operando1 << ":" << endl << endl;
+            for (operando2 = 0; operando2 <= 10; operando2++) {
+                resultado = operando1 * operando2;
+                cout << operando1 << " x " << operando2 << " = " << resultado << endl;
+            }
+            break;
+
+        case '3':
+            system("cls");
+            cout << "Saindo do programa." << endl;
+            continuar = false;
+			break;
+
+
+        default:
+            cout << "Opção inválida. Por favor, escolha 1 ou 2." << endl;
+            break;
+
+        }
+
+    }
     return 0;
 }
 
