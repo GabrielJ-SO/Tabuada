9eb7fe4fc3020d902994b3651dad2fbfd0b7f68d
GabrielJ-SO <gabrielodisi20@gmail.com>
2025-07-16 15:23:52 -0300

    loop corrigido ao digitar letras e caracteres especiais em case 2

--- Tabuada.cpp
+++ Tabuada.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <clocale>
+
 using namespace std;
 
 int main()
 {
     system("chcp 65001"); // muda o código do console para UTF-8
+	system("cls");
 
     float operando1, operando2, resultado;
     char opcao;
-	bool continuar = true, entrada = false;
-
+    bool continuar = true;
 
     while (continuar)
     {
         cout << "\tCALCULAR TABUADA ☺" << endl;
         cout << "O que você prefere?" << endl;
         cout << "1. Gerar a tabuada do 1 ao 10." << endl;
-	    cout << "2. Gerar a tabuada de um número específico." << endl;
+        cout << "2. Gerar a tabuada de um número específico." << endl;
         cout << "3. Sair." << endl;
-	    cout << "Digite a opção desejada: ";
+        cout << "Digite a opção desejada: ";
         cin >> opcao;
-
 
         switch (opcao)
         {
@@ -35,42 +34,41 @@ int main()
                 }
                 cout << endl;
             }
-
+            cout << endl << endl;
             break;
 
         case '2':
             system("cls");
-
-            cout << "Digite o número para gerar a tabuada:" << endl;
+            cout << "Digite um número para gerar a tabuada." << endl;
             cout << "Número: ";
-            cin >> operando1;
-            
-            cout << "\tTabuada do " << operando1 << ":" << endl << endl;
+
+			while (!(cin >> operando1)) // Verificação para evitar loop e garantir entrada válida.
+            {
+                cin.clear(); // Limpa o estado de erro do cin
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignora entrada inválida
+                cout << "Entrada inválida. Por favor, digite um número para calcular a tabuada: ";
+            }
+
+            cout << "\n\tTabuada do " << operando1 << ":" << endl << endl;
             for (operando2 = 0; operando2 <= 10; operando2++) {
                 resultado = operando1 * operando2;
                 cout << operando1 << " x " << operando2 << " = " << resultado << endl;
             }
-            
+			cout << endl << endl;
             break;
 
         case '3':
             system("cls");
             cout << "Saindo do programa." << endl;
             continuar = false;
-			
             break;
-
 
         default:
-			system("cls");
+            system("cls");
             cout << "Opção inválida. Por favor, escolha 1 ou 2." << endl;
-			system("pause");
-
+            system("pause");
             break;
         }
-
     }
     return 0;
 }
-
-
